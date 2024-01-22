f9431ea47de7f2f13a090a3d78066af6b194fcc4
Gautamseema <seematest244@gmail.com>
2024-01-22 15:09:54 +0000

    serenityHW

---
+++ src/test/java/com/gorest/testbase/TestBase.java
@@ -0,0 +1,22 @@
+package com.gorest.testbase;
+
+
+import com.gorest.constant.Path;
+import com.gorest.utils.PropertyReader;
+import io.restassured.RestAssured;
+import org.junit.BeforeClass;
+
+/**
+ * Created by SG
+ */
+public class TestBase {
+    public static PropertyReader propertyReader;
+
+    @BeforeClass
+    public static void init() {
+        propertyReader = PropertyReader.getInstance();
+        RestAssured.baseURI = propertyReader.getProperty("baseUrl");
+        RestAssured.basePath = Path.USERS;
+    }
+
+}
